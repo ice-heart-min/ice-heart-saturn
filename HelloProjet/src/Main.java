743155b20a48e949f7d65f86b2241f665be49abe
1095568764@qq.com <Zjm.52580>
2019-12-19 20:34:12 +0800

    B用户的项目

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class Main {
 
     @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+    public void bbb() {
+        System.out.println("bbb!");
     }
 }

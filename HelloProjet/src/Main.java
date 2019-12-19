89f1d7a00cfcbe0e3ed6feed56149ff63cd51766
1095568764@qq.com <Zjm.52580>
2019-12-19 20:39:35 +0800

    B用户的项目

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -2,16 +2,6 @@ import org.junit.Test;
 
 public class Main {
 
-    @Test
-    public void bbb() {
-        System.out.println("bbb!");
-    }
-
-   @Test
-    public void aaa() {
-        System.out.println("aaa!");
-    }
-
  @Test
     public void ccc() {
         System.out.println("aaa!");
